572538875308b3e1858ff3ac67c58fdc9c02dd49
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-04-23 22:17:09 -0300

    feat: commented codes for later review and add kind_id as mandatory parameter

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,21 @@
 class Contact < ApplicationRecord
-  belongs_to :kind
+  belongs_to :kind#, optional: true
+
+  # def author
+  #   'Lucas Guimarães'
+  # end
+
+  # def kind_description
+  #   self.kind.description
+  # end
+
+  # def as_json(options={})
+  #   super(
+  #     root: true,
+  #     methods: [:kind_description, :author],
+  #     include: { kind: { only: :description } }
+  #   )
+  # end
+
+
 end
