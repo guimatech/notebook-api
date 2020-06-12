67705b901e6d5d9209855d32d46dfdf6e37e6916
Lucas S. Guimarães <lucasasafe@outlook.com.br>
2020-06-11 21:28:14 -0300

    feat: add default links in json with url "related" of kind

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { kind_url(object.kind.id) }
+  end
   has_many :phones, dependent: :destroy
   has_one :address, dependent: :destroy
+
+  # link(:self) { contact_url(object.id) }
+  # link(:kind) { kind_url(object.kind.id) }
 
   def attributes(*args)
     h = super(*args)
